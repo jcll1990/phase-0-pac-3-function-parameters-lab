d290f8914128da410c08b986bd4397dcecbac974
Juan Carlos Lopez Larco <Jclopezlarco@gmail.com>
2023-08-08 14:50:59 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,14 @@
+// 1
+function introduction(name) {
+    return `Hi, my name is ${name}.`
+    }
+
+// 2
+function introductionWithLanguage(name, language) {
+return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+//3
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
